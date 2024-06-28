168c33d7627d0453843e52b8a5e654d6072042d6
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-06-28 10:00:54 -0400

    Added user input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// add ability to accept user input
+use std::io; 
+
 fn main() {
-    println!("Hello, world!");
+    // println prints to the terminal/screen
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // store user input into a string
+    // mut = mutable
+    // variables in rust are immutable by default
+    // making them unchangable once a value is assigned
+    // thus add mut will allow changes
+    let mut guess = String::new();
+
+    // handle the user input using the io library
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // print the user input
+    println!("You guessed: {}", guess);
 }
